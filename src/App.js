9e858fba7ba6d3d5275e15e806b8e7b2cbb9fd6e
LeDucThuan <dthuanle.20@gmail.com>
2019-09-14 21:47:39 +0700

    modify App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import card from './components/card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Card src="https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi1zKnLwtDkAhUGFogKHbsfCSAQjRx6BAgBEAQ&url=https%3A%2F%2Fweheartit.com%2Fentry%2F329116733&psig=AOvVaw18OUKEjee8__Rtwrwz-WAe&ust=1568557602808737" 
+              mainContent="Tanjiro Kamado"
+              description="Tanjiro Kamado (竈門かまど炭たん治じ郎ろう Kamado Tanjirō?) is the main protagonist of the manga and anime, Demon Slayer: Kimetsu no Yaiba."
+              />
+      </div>
+    );
+  }
 }
 
 export default App;
